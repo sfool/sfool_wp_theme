646fe827957b1e9a9147d1dbcea13e2c29d29a79
yuuki-ha <yuki13002600@gmail.com>
2013-10-17 19:43:19 +0900

    最初のコミット

---
+++ tpl-posts_link_1.php
@@ -0,0 +1,11 @@
+<div class="post_nav_area">
+<ul class="post_nav">
+<li class="post_prev">
+<?php next_posts_link('<span class="post_prev_text">Old Entries</span>'); ?>
+</li>
+<li class="post_next">
+<?php previous_posts_link('<span class="post_next_text">New Entries</span>'); ?>
+</li>
+</ul>
+<!-- end .post_nav_area -->
+</div>
